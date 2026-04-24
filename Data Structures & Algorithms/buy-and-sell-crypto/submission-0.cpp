7794b22b27c9ba22d7426e6afb3bb226dbe81e15
Sandeep Pal <sanpal1997@gmail.com>
2026-04-24 17:28:07 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int ans = 0;
+
+        for (int i=1; i<prices.size(); i++) {
+            ans = max(ans, max(0, prices[i] - buy));
+
+            buy = min(buy, prices[i]);
+        }
+
+        return ans; 
+
+    }
+};
