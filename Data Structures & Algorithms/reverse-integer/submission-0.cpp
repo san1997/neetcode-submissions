f523be0dcf93d87401373402b0405544ab524f43
Sandeep Pal <sanpal1997@gmail.com>
2026-05-14 20:10:40 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int n = 0;
+
+        while (x) {
+            n = 10*n + x%10;
+
+            if (n > INT_MAX || n < INT_MIN) {
+                return 0;
+            }
+            x = x / 10;
+        }
+
+        return n;
+    }
+};
