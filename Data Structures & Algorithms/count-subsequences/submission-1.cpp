8422460a8b8bc592f58f4eedc491815bef4d52ae
Sandeep Pal <sanpal1997@gmail.com>
2026-05-19 20:27:15 +0530

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[1004][1004] = {0};
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        dp[m][n] = 1;
+        for (int i=0; i<m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i=m-1; i>=0; i--) {
+            for (int j=n-1; j>=0; j--) {
+                dp[i][j] = dp[i+1][j];
+
+                if (s[i] == t[j]) {
+                    dp[i][j] += dp[i+1][j+1];
+                }
+            }
+        }
+
+
+        return dp[0][0];
+
+    }
+};
